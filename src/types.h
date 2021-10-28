c395d782dfe494081b312ce251ee17d6288f6cb6
zhongji <featherfly@foxmail.com>
2021-10-28 16:19:33 +0800

    types直接引入stdint.h和stdbool.h

--- src/types.h
+++ src/types.h
@@ -1,6 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef byte
+typedef unsigned char byte;
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,49 +16,8 @@ extern "C" {
 #define nullptr NULL
 #endif
 
-#ifndef bool
-typedef unsigned int bool;
-#endif
-
-#ifndef true
-#define true 1
-#endif
-
-#ifndef false
-#define false 0
-#endif
-
-#endif
-
-#ifndef byte
-typedef unsigned char byte;
-#endif
-
-#ifndef int8_t
-typedef char int8_t;
-#endif
-
-#ifndef uint8_t
-typedef unsigned char uint8_t;
-#endif
-
-#ifndef int16_t
-typedef short int16_t;
-#endif
-
-#ifndef uint16_t
-typedef unsigned short uint16_t;
-#endif
-
-#ifndef int32_t
-typedef int int32_t;
-#endif
-
-#ifndef uint32_t
-typedef unsigned int uint32_t;
-#endif
-
-
 #ifdef __cplusplus
 }
+#endif
+
 #endif
