3223a35629f6d6c1dba71981511173559a7cef14
zhongji <featherfly@foxmail.com>
2022-10-11 15:28:11 +0800

    feat: 加入ARRAY_EQUALS_FUN

--- src/arrays.c
+++ src/arrays.c
@@ -1,60 +1,57 @@
 
 #include "arrays.h"
 
-bool array_equals_uint8_char(const uint8_t source[], const char target[], uint32_t size)
-{
-  bool eq = true;
-  for (int i = 0; i < size; i++)
-  {
-    if (source[i] != target[i]) {
-      return false;
+ARRAY_EQUALS_FUN(array_equals_uint32, uint32_t, uint32_t);
+
+ARRAY_EQUALS_FUN(array_equals_uint8_char, uint8_t, char);
+
+// bool array_equals_uint8_char(const uint8_t source[], const char target[],
+//                              uint32_t size) {
+//     bool eq = true;
+//     for (int i = 0; i < size; i++) {
+//         if (source[i] != target[i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+bool array_equals(const char source[], const char target[], uint32_t size) {
+    bool eq = true;
+    for (int i = 0; i < size; i++) {
+        if (source[i] != target[i]) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
-bool array_equals(const char source[], const char target[], uint32_t size)
-{
-  bool eq = true;
-  for (int i = 0; i < size; i++)
-  {
-    if (source[i] != target[i]) {
-      return false;
+
+int array_copy_uint8(const uint8_t source[], uint8_t target[], uint32_t size) {
+    int index;
+    for (index = 0; index < size; index++) {
+        target[index] = source[index];
     }
-  }
-  return true;
+    return index;
 }
 
-int array_copy_uint8(const uint8_t source[], uint8_t target[], uint32_t size)
-{  
-  int index;
-  for(index = 0; index < size; index++) 
-  {
-    target[index] = source[index];
-  } 
-  return index;
+int array_copy_char(const char source[], char target[], uint32_t size) {
+    return array_copy_uint8((uint8_t *)source, (uint8_t *)target, size);
 }
 
-int array_copy_char(const char source[], char target[], uint32_t size)
-{
-  return array_copy_uint8((uint8_t *) source, (uint8_t *) target, size);
+int array_copy_range_uint8(const uint8_t source[], uint8_t target[],
+                           uint32_t start, uint32_t size) {
+    int index;
+    for (index = start; index < size + start; index++) {
+        target[index - start] = source[index];
+    }
+    return index;
 }
 
-int array_copy_range_uint8(const uint8_t source[], uint8_t target[], uint32_t start, uint32_t size)
-{
-  int index;
-  for(index = start; index < size + start; index++) 
-  {
-    target[index - start] = source[index];
-  } 
-  return index;
-}
-
-int array_set(char target[], const char source[], uint32_t start, uint32_t len)
-{
-  int index;
-  for(index = 0; index < len; index++) 
-  {
-    target[start + index] = source[index];
-  }
-  return index;
+int array_set(char target[], const char source[], uint32_t start,
+              uint32_t len) {
+    int index;
+    for (index = 0; index < len; index++) {
+        target[start + index] = source[index];
+    }
+    return index;
 }
--- src/arrays.h
+++ src/arrays.h
@@ -1,19 +1,30 @@
 #ifndef ARRAYS_H
 #define ARRAYS_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool array_equals_uint8_char(const uint8_t source[], const char target[], uint32_t len);
+#define ARRAY_EQUALS_FUN(fn_name, st, tt) bool fn_name(const st source[], const tt target[], uint32_t len) {\
+    bool eq = true;                                                                                         \
+    for (int i = 0; i < len; i++) {                                                                         \
+        if (source[i] != target[i]) {                                                                       \
+            return false;                                                                                   \
+        }                                                                                                   \
+    }                                                                                                       \
+    return true;                                                                                            \
+}                                                 
+
+bool array_equals_uint8_char(const uint8_t source[], const char target[],
+                             uint32_t len);
 
 bool array_equals(const char source[], const char target[], uint32_t len);
 
 /**
- * @param source 复制源 
+ * @param source 复制源
  * @param target 复制目标
  * @param size 复制数量
  */
@@ -22,16 +33,17 @@ int array_copy_uint8(const uint8_t source[], uint8_t target[], uint32_t size);
 int array_copy_char(const char source[], char target[], uint32_t size);
 
 /**
- * @param source 复制源 
+ * @param source 复制源
  * @param target 复制目标
  * @param start 复制源开始索引（包含）
  * @param size 复制数量
  */
-int array_copy_range_uint8(const uint8_t source[], uint8_t target[], uint32_t start, uint32_t size);
+int array_copy_range_uint8(const uint8_t source[], uint8_t target[],
+                           uint32_t start, uint32_t size);
 
 /**
  * @param target 设置目标
- * @param source 设置源 
+ * @param source 设置源
  * @param start 目标起始位置
  * @param len 源数组长度
  */
