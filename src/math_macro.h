fde239b7731d11afe27589bcf6c78f88ee5b281a
zhongj <featherfly@foxmail.com>
2022-10-13 14:56:36 +0800

    docs: 为宏定义加入说明文档

--- src/math_macro.h
+++ src/math_macro.h
@@ -7,6 +7,18 @@
 extern "C" {
 #endif
 
+/**
+ * @brief define pow function macro.
+ * @code 
+ *   // define pow function
+ *   POW_FULL_FUN(pow_uint32, uint32_t, uint32_t);
+ *   // invoke the defined function
+ *   uint32_t result = pow_uint32(2, 2);
+ * @endcode
+ * @param fn_name function name
+ * @param t_x value type (example: uint32_t)
+ * @param t_return return value type (example: uint32_t)
+ */
 #define POW_FULL_FUN(fn_name, t_x, t_return) t_return fn_name(t_x x, uint32_t y) { \
     t_return ret = (t_return) 1;                                                   \
     for (uint32_t i = 0; i < y; i++) {                                             \
@@ -15,13 +27,24 @@ extern "C" {
     return ret;                                                                    \
 }
 
+/**
+ * @brief define pow function macro.
+ * @code 
+ *   // define pow function
+ *   POW_FUN(pow_uint32, uint32_t);
+ *   // invoke the defined function
+ *   uint32_t result = pow_uint32(2, 2);
+ * @endcode
+ * @param fn_name function name
+ * @param t_x value type (example: uint32_t)
+ */
 #define POW_FUN(fn_name, t_x) POW_FULL_FUN(fn_name, t_x, t_x)
 
 // POW_FULL_FUN(pow_int, uint32_t, uint32_t);
 
 // POW_FUN(pow_int, uint32_t);
 
-POW_FULL_FUN(pow_long, uint64_t, uint64_t);
+// POW_FULL_FUN(pow_long, uint64_t, uint64_t);
 
 #ifdef __cplusplus
 }
