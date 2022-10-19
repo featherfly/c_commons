6e11d52833402a3aed5b7a28a13444d77397af7e
zhongj <featherfly@foxmail.com>
2022-10-19 16:46:18 +0800

    refactor: 头文件移动到include文件夹

---
+++ include/numbers.h
@@ -0,0 +1,74 @@
+#ifndef NUMBERS_H
+#define NUMBERS_H
+
+#include <stdint.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// #define INT_TO_BYTE_FUN(fn_name, t_int, len) void fn_name(t_int i, uint8_t abyte[]) {\
+//     int j = 0;                                                                       \
+//     for (int i = len - 1 ; i >= 0; i--) {                                            \
+//         abyte[i] = (uint8_t)(( t_int )(0xff * pow(256, j)) & i);                     \
+//         j++;                                                                         \
+//     }                                                                                \
+// }                                                                                    \
+
+/**
+ * @brief int8_t to uint8_t array[8]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 8
+ */
+void int64_to_bytes(int64_t value, uint8_t bytes[]);
+
+/**
+ * @brief int8_t to uint8_t array[6]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 6
+ */
+void int48_to_bytes(int64_t value, uint8_t bytes[]);
+
+/**
+ * @brief int8_t to uint8_t array[4]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 4
+ */
+void int32_to_bytes(int32_t value, uint8_t bytes[]);
+
+/**
+ * @brief int8_t to uint8_t array[3]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 3
+ */
+void int24_to_bytes(int32_t value, uint8_t bytes[]);
+
+/**
+ * @brief int8_t to uint8_t array[2]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 2
+ */
+void int16_to_bytes(int16_t value, uint8_t bytes[]);
+
+/**
+ * @brief int8_t to uint8_t array[1]
+ * 
+ * @param value the value to convert
+ * @param bytes the byte array, length 1
+ */
+void int8_to_bytes(int8_t value, uint8_t bytes[]);
+
+void to_hex(uint8_t c, char* hex);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/numbers.c
+++ src/numbers.c
@@ -2,14 +2,12 @@
 
 #include <stdio.h>
 
-static char _hex_chars[] = {
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
+static char _hex_chars[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 // INT_TO_BYTE_FUN(int64_to_bytes, int64_t, 8);
 
-void int64_to_bytes(int64_t i, uint8_t abyte[])
-{
+void int64_to_bytes(int64_t i, uint8_t abyte[]) {
     abyte[7] = (uint8_t)(0xff & i);
 
     abyte[6] = (uint8_t)((0xff00 & i) >> 8);
@@ -27,8 +25,7 @@ void int64_to_bytes(int64_t i, uint8_t abyte[])
     abyte[0] = (uint8_t)((0xff00000000000000 & i) >> 56);
 }
 
-void int48_to_bytes(int64_t i, uint8_t abyte[])
-{
+void int48_to_bytes(int64_t i, uint8_t abyte[]) {
     abyte[5] = (uint8_t)(0xff & i);
 
     abyte[4] = (uint8_t)((0xff00 & i) >> 8);
@@ -42,8 +39,7 @@ void int48_to_bytes(int64_t i, uint8_t abyte[])
     abyte[0] = (uint8_t)((0xff0000000000 & i) >> 40);
 }
 
-void int32_to_bytes(int32_t i, uint8_t abyte[])
-{
+void int32_to_bytes(int32_t i, uint8_t abyte[]) {
     abyte[3] = (uint8_t)(0xff & i);
 
     abyte[2] = (uint8_t)((0xff00 & i) >> 8);
@@ -53,8 +49,7 @@ void int32_to_bytes(int32_t i, uint8_t abyte[])
     abyte[0] = (uint8_t)((0xff000000 & i) >> 24);
 }
 
-void int24_to_bytes(int32_t i, uint8_t abyte[])
-{
+void int24_to_bytes(int32_t i, uint8_t abyte[]) {
     abyte[2] = (uint8_t)(0xff & i);
 
     abyte[1] = (uint8_t)((0xff00 & i) >> 8);
@@ -62,24 +57,20 @@ void int24_to_bytes(int32_t i, uint8_t abyte[])
     abyte[0] = (uint8_t)((0xff0000 & i) >> 16);
 }
 
-void int16_to_bytes(int16_t i, uint8_t abyte[])
-{
+void int16_to_bytes(int16_t i, uint8_t abyte[]) {
     abyte[1] = (uint8_t)(0xff & i);
 
     abyte[0] = (uint8_t)((0xff00 & i) >> 8);
 }
 
-void int8_to_bytes(int8_t i, uint8_t abyte[])
-{
+void int8_to_bytes(int8_t i, uint8_t abyte[]) {
     abyte[0] = (uint8_t)(0xff & i);
 }
-    
-void to_hex(uint8_t c, char* hex)
-{
-    if (c < 16)
-    {
+
+void to_hex(uint8_t c, char* hex) {
+    if (c < 16) {
         sprintf(hex, "0%c", _hex_chars[c]);
-    } else{
+    } else {
         sprintf(hex, "%X", c);
     }
 }
